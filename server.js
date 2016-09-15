0dea823d89d3b76c3d30a8fcf9a1c7b095d60ab8
HK-SERVER <wangxuxiao@jxat.cn>
2016-09-15 20:16:49 +0800

    server 8080

--- server.js
+++ server.js
@@ -30,113 +30,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 
-app.get('/mysql/get/:table/:indexdata/', function(req, res){ 
-      var  actionSql = "SELECT * FROM "+req.params.table+" LIMIT " + ((req.params.indexdata-1)*10) + ",10";     
-//查 query
-conn.query(actionSql,function (err, result) {
-        if(err){
-         // $("#dbif").text('[SELECT ERROR] - '+err.message);//("MYSQL:127.0.0.1<br>端口:3306");
-          //console.log('[SELECT ERROR] - ',err.message);
-          if(err.code === 'PROTOCOL_CONNECTION_LOST') conn.connect();
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          return;
-        }  
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(''+JSON.stringify(result)+'');     
-        });
-     }); 
-
-app.get('/mysql/stdzin/:days/:indexdata/', function(req, res){ 
-      var  actionSql = "SELECT * FROM stin WHERE DAY>="+req.params.days+" LIMIT " + ((req.params.indexdata-1)*10) + ",10";     
-//查 query
-conn.query(actionSql,function (err, result) {
-        if(err){
-         // $("#dbif").text('[SELECT ERROR] - '+err.message);//("MYSQL:127.0.0.1<br>端口:3306");
-          //console.log('[SELECT ERROR] - ',err.message);
-          if(err.code === 'PROTOCOL_CONNECTION_LOST') conn.connect();
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          return;
-        }  
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(''+JSON.stringify(result)+'');     
-        });
-     }); 
-
-app.get('/mysql/stdzincount/:days/', function(req, res){ 
-      var  actionSql = "select COUNT(*) as count FROM stin WHERE DAY>="+req.params.days+"";     
-//查 query
-conn.query(actionSql,function (err, result) {
-        if(err){
-         // $("#dbif").text('[SELECT ERROR] - '+err.message);//("MYSQL:127.0.0.1<br>端口:3306");
-          //console.log('[SELECT ERROR] - ',err.message);
-          if(err.code === 'PROTOCOL_CONNECTION_LOST') conn.connect();
-          res.writeHead(200, {'Content-Type': 'application/json'});          
-          return;
-        }  
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(''+JSON.stringify(result)+'');     
-        });
-     }); 
-
-
-app.get('/mysql/count/:table/', function(req, res){ 
-      var  actionSql = "select COUNT(*) as count from "+req.params.table+"";     
-//查 query
-conn.query(actionSql,function (err, result) {
-        if(err){
-         // $("#dbif").text('[SELECT ERROR] - '+err.message);//("MYSQL:127.0.0.1<br>端口:3306");
-          //console.log('[SELECT ERROR] - ',err.message);
-          if(err.code === 'PROTOCOL_CONNECTION_LOST') conn.connect();
-          res.writeHead(200, {'Content-Type': 'application/json'});          
-          return;
-        }  
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(''+JSON.stringify(result)+'');     
-        });
-     }); 
-
-
-app.post('/mysql/post/:fun/', function(req, res){ 
-
-    if("updatadevicelistinfo"===req.params.fun)
-    {
-      var  actionSql = "call updatadevicelistinfo('"+req.body.request.devid+"','"+req.body.request.devcode+"','"+req.body.request.devarea+"')";     
-    }
-    else if("updatacardlistinfo"===req.params.fun)
-    {
-      var  actionSql = "call updatacardlistinfo('"+req.body.request.cardid+"','"+req.body.request.cardcode+"','"+req.body.request.carduserid+"')"; 
-    }
-    else if("updatauserlistinfo"===req.params.fun)
-    {
-      var  actionSql = "call updatauserlistinfo('"+req.body.request.userid+"','"+req.body.request.username+"','"+req.body.request.userno+"','"+req.body.request.userunit+"')"; 
-      console.log(req.body.request);
-    }
-
-    else
-    {var  actionSql = "select COUNT(*) as count from "+req.params.table+""; }
-          
-//查 query
-conn.query(actionSql,function (err, result) {
-        if(err){
-         // $("#dbif").text('[SELECT ERROR] - '+err.message);//("MYSQL:127.0.0.1<br>端口:3306");
-          //console.log('[SELECT ERROR] - ',err.message);
-          if(err.code === 'PROTOCOL_CONNECTION_LOST') conn.connect();
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          return;
-        }  
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(''+JSON.stringify(result)+'');     
-        });
-
-
-     }); 
-
-
 
 
 //bind the server to the 80 port
 //server.listen(3000);//for local test
-server.listen(process.env.PORT || 3000);//publish to heroku
+server.listen(process.env.PORT || 8080);//publish to heroku
 //server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
 //console.log('server started on port'+process.env.PORT || 3000);
 //handle the socket
